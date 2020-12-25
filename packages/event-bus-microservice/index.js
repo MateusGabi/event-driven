b0cbc5879848669a22721b6385f8bcbeb4a43604
Mateus <mateusgabimoreira@gmail.com>
2020-12-25 18:46:07 -0300

    added registry

--- packages/event-bus-microservice/index.js
+++ packages/event-bus-microservice/index.js
@@ -6,26 +6,46 @@ class Microservice {
     this.eventBus = Object.assign(
       {
         uri: "http://localhost",
-        port: 8081
+        port: 8081,
+        name: "#DEFAULT_NAME",
       },
       eventBus
     );
     this.socket = io(`${this.eventBus.uri}:${this.eventBus.port}`);
+    this.instanceId =
+      Math.random().toString(32).substring(2) +
+      Math.random().toString(32).substring(2) +
+      Math.random().toString(32).substring(2);
+
+    doRegistry();
+  }
+
+  doRegistry() {
+    this.sendEvent("REGISTRY_SERVICE", {
+      name: this.name,
+      instanceId: this.instanceId,
+    });
   }
 
   listenEvent(event, callback) {
     this.socket.on(event, callback);
+    this.socket.on("KEEP_ALIVE_EVENT", function () {
+      this.sendEvent("KEEP_ALIVE_EVENT_RESPONSE", {
+        name: this.name,
+        instanceId: this.instanceId,
+      });
+    });
   }
 
   sendEvent(type, payload) {
     console.log("send event", {
       type,
-      payload
+      payload,
     });
 
     axios.post(`${this.eventBus.uri}:${this.eventBus.port}/publish`, {
       type,
-      payload
+      payload,
     });
   }
 }
